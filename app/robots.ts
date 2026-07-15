0c82db21b6daeae06ddcd7621b666c82c625ddbb
ssshawn-24 <shawnwng630@gmail.com>
2026-07-15 14:50:10 +0800

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/_next/'],
     },
-    sitemap: 'https://www.moran-growth.site/sitemap.xml',
+    sitemap: 'https://moran-growth.site/sitemap.xml',
   }
 }
