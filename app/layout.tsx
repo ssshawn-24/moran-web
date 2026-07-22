7c40e899cb01f080d16e6c5c376ea30cd34cf4d4
ssshawn-24 <shawnwng630@gmail.com>
2026-07-22 15:09:18 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN" className="antialiased" style={{ fontSmooth: "always" }}>
+      <head>
+        {/* GA4 代码 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-你的MeasurementID"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-你的MeasurementID');
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
